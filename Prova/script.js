27dc009864941de2e61d20d91dc6107a958a94e1
joaoVicDuraes <joao.vod06@gmail.com>
2025-04-11 20:52:43 -0300

    continuação do projeto

--- Prova/script.js
+++ Prova/script.js
@@ -1,31 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("#form-contato");
-    const feedback = document.querySelector("#feedback");
+  const form = document.querySelector("#form-contato"); 
+  form.setAttribute("action", "#");
+  const feedback = document.querySelector("#feedback");
   
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
   
-      const nome = document.querySelector("#nome").value.trim();
-      const email = document.querySelector("#email").value.trim();
-      const mensagem = document.querySelector("#mensagem").value.trim();
+    const nome = document.querySelector("#nome").value.trim();
+    const email = document.querySelector("#email").value.trim();
+    const mensagem = document.querySelector("#mensagem").value.trim();
   
-      if (!nome || !email || !mensagem) {
-        feedback.textContent = "Por favor, preencha todos os campos.";
-        feedback.style.color = "yellow";
-        return;
-      }
+    if (!nome || !email || !mensagem) {
+      feedback.textContent = "Por favor, preencha todos os campos.";
+      feedback.style.color = "yellow";
+      return;
+    }
   
-      const emailVerificar = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
-      if (!emailVerificar.test(email)) {
-        feedback.textContent = "Digite um e-mail válido.";
-        feedback.style.color = "yellow";
-        return;
-      }
+   const emailVerificar = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+   
+    if (!emailVerificar.test(email)) {
+      feedback.textContent = "Digite um e-mail válido.";
+      feedback.style.color = "yellow";
+      return;
+  }
   
-
-      feedback.textContent = "Mensagem enviada com sucesso!";
-      feedback.style.color = "lightgreen";
-        form.reset();
-        });
-    
+    feedback.textContent = "Mensagem enviada com sucesso!";
+    feedback.style.color = "lightgreen";
+  });
 });
+
+
+
+
