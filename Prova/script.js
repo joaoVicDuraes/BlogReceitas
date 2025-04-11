0f6b7fad5b74fb0db8995378bc033cd6bfb7b24d
João Victor <joao.vod06@gmail.com>
2025-04-11 19:54:47 -0300

    Criação da pagina web de um site de Receitas

---
+++ Prova/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#form-contato");
+    const feedback = document.querySelector("#feedback");
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      const nome = document.querySelector("#nome").value.trim();
+      const email = document.querySelector("#email").value.trim();
+      const mensagem = document.querySelector("#mensagem").value.trim();
+  
+      if (!nome || !email || !mensagem) {
+        feedback.textContent = "Por favor, preencha todos os campos.";
+        feedback.style.color = "yellow";
+        return;
+      }
+  
+      const emailVerificar = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+      if (!emailVerificar.test(email)) {
+        feedback.textContent = "Digite um e-mail válido.";
+        feedback.style.color = "yellow";
+        return;
+      }
+  
+
+      feedback.textContent = "Mensagem enviada com sucesso!";
+      feedback.style.color = "lightgreen";
+        form.reset();
+        });
+    
+});
